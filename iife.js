d8139fe5a2bed4328d3e573ed533c7a9a1a1d0e8
Ryan Silva <RyandotSilva@gmail.com>
2019-05-03 17:29:02 -0400

    Solve closure bug using iife

--- iife.js
+++ iife.js
@@ -1,25 +1,45 @@
-const sayHello = (function() {
-	const message = 'Hello';
+// const sayHello = (function() {
+// 	const message = 'Hello';
 
-	function sayHello() {
-		console.log(message);
+// 	function sayHello() {
+// 		console.log(message);
+// 	}
+// 	return sayHello;
+// })();
+
+// const counter = (function() {
+// 	let count = 0;
+
+// 	return {
+// 		inc: function() {
+// 			count++;
+// 		},
+// 		get: function() {
+// 			console.log(count);
+// 		}
+// 	};
+// })();
+
+// counter.get();
+// counter.inc();
+// counter.get();
+
+function makeFunctionArray() {
+	const arr = [];
+
+	for (var i = 0; i < 5; i++) {
+		arr.push(
+			(function(x) {
+				return function() {
+					console.log(x);
+				};
+			})(i)
+		);
 	}
-	return sayHello;
-})();
 
-const counter = (function() {
-	let count = 0;
+	return arr;
+}
 
-	return {
-		inc: function() {
-			count++;
-		},
-		get: function() {
-			console.log(count);
-		}
-	};
-})();
+const functionArr = makeFunctionArray();
 
-counter.get();
-counter.inc();
-counter.get();
+functionArr[0](); // shows 5
