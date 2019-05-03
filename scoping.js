58f3e584299a7865a31a0412f0dc319f2445c3cf
Ryan Silva <RyandotSilva@gmail.com>
2019-05-03 16:46:54 -0400

    Add hoisting examples

--- scoping.js
+++ scoping.js
@@ -1,5 +1,9 @@
-console.log(thisIsAConst); // error
-console.log(thisIsALet); // error
+// console.log(thisIsAConst); // error
+// console.log(thisIsALet); // error
+
+thisIsHoisted();
+thisIsNotHoisted(); // error not defined
+isThisHoisted(); // error type error not a function
 
 const thisIsAConst = 50;
 
@@ -19,4 +23,18 @@ thisIsAVar = 51;
 
 var thisIsAVar = 'new value'; // no error (shadowing example)
 
-console.log(thisIsAVar); // new value
+// console.log(thisIsAVar); // new value
+
+// Function Hoisting
+function thisIsHoisted() {
+	console.log('This is a function declared at the bottom of a file');
+}
+
+// Function not hoisted declared as constant so it can't be changed
+const thisIsNotHoisted = function() {
+	console.log('Should this be hoisted?');
+};
+
+var isThisHoisted = function() {
+	console.log('Is this hoisted?');
+};
