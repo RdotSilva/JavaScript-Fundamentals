3482bc14758d8e0682c78d1fa8a4a18663a3d763
Ryan Silva <RyandotSilva@gmail.com>
2019-05-03 16:31:13 -0400

    Add var examples

--- scoping.js
+++ scoping.js
@@ -13,3 +13,10 @@ let thisIsALet = 51;
 thisIsALet = 50;
 
 // let thisIsALet = 51 // errors!
+
+var thisIsAVar = 50;
+thisIsAVar = 51;
+
+var thisIsAVar = 'new value'; // no error (shadowing example)
+
+console.log(thisIsAVar); // new value
