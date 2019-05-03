bf379962645da18ec5dcf1d754405d268b3adc9e
Ryan Silva <RyandotSilva@gmail.com>
2019-05-03 17:10:28 -0400

    Add closure example

--- closure.js
+++ closure.js
@@ -1,14 +1,31 @@
-function makeFunctionArray() {
-	const arr = [];
+// function makeFunctionArray() {
+// 	const arr = [];
 
-	for (var i = 0; i < 5; i++) {
-		arr.push(function() {
-			console.log(i);
-		});
+// 	for (var i = 0; i < 5; i++) {
+// 		arr.push(function() {
+// 			console.log(i);
+// 		});
+// 	}
+// 	return arr;
+// }
+
+// const functionArr = makeFunctionArray();
+
+// functionArr[0](); // shows 5
+
+function makeHelloFunction() {
+	const message = 'Hello';
+
+	function sayHello() {
+		console.log(message);
 	}
-	return arr;
+	return sayHello;
 }
 
-const functionArr = makeFunctionArray();
+const sayHello = makeHelloFunction();
 
-functionArr[0](); // shows 5
+// console.log(message); // Error message not defined
+console.log('typeof message', typeof message);
+console.log(sayHello.toString());
+
+sayHello();
