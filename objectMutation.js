c5548d6f72b3d4e70832163a0a469bb6c0166950
Ryan Silva <RyandotSilva@gmail.com>
2019-05-03 15:53:07 -0400

    Add deep copy example

--- objectMutation.js
+++ objectMutation.js
@@ -8,10 +8,35 @@ const o = {
 
 const o2 = Object.assign({}, o); // shallow copy
 
-o2.obj.key = 'new value';
+// o2.obj.key = 'new value';
 
-console.log(o.obj.key); // new value
+// console.log(o.obj.key); // new value
 
-o.a = 'new value';
+// o.a = 'new value';
 
-console.log(o2.a); // new value
+// console.log(o2.a); // new value
+
+// Deep Copy
+function deepCopy(obj) {
+	// Check if values are objects
+	// If so copy that object (deep copy)
+	// Else return the value
+	const keys = Object.keys(obj);
+
+	const newObject = {};
+
+	for (let i = 0; i < keys.length; i++) {
+		const key = keys[i];
+		if (typeof obj[key] === 'object') {
+			newObject[key] = deepCopy(obj[key]);
+		} else {
+			newObject[key] = obj[key];
+		}
+	}
+	return newObject;
+}
+
+const o3 = deepCopy(o);
+
+o.obj.key = 'new key!';
+console.log(o3.obj.key);
