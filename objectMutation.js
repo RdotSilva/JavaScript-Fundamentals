a104295731b96f76a0bf54602ccbe63dc5678529
Ryan Silva <RyandotSilva@gmail.com>
2019-05-03 15:34:12 -0400

    Add shallow copy example

--- objectMutation.js
+++ objectMutation.js
@@ -1,9 +1,16 @@
 const o = {
 	a: 'a',
-	b: 'b'
+	b: 'b',
+	obj: {
+		key: 'key'
+	}
 };
 
-const o2 = o;
+const o2 = Object.assign({}, o); // shallow copy
+
+o2.obj.key = 'new value';
+
+console.log(o.obj.key); // new value
 
 o.a = 'new value';
 
