a4a20af69aae5b86f06aa69d530c2d37f2fb47a0
Ryan Silva <RyandotSilva@gmail.com>
2019-05-06 20:36:15 -0400

    Add arrow function notation example

--- this.js
+++ this.js
@@ -19,4 +19,14 @@ friend.greet(); // this.name = david
 const greet = person.greet.bind({ name: 'this is a bound object' });
 person.greet.call({ name: 'this is a bound object' });
 person.greet.apply({ name: 'this is a bound object' });
+
+const newPerson = {
+	name: 'newPerson',
+	greet: () => {
+		console.log('hi, ' + this.name);
+	}
+};
+
+newPerson.greet(); // undefined global object of this
+
 greet();
