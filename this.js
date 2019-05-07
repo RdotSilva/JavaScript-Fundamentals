27c50a77dba29d75def6a45c0ce5f7e0cf0e103a
Ryan Silva <RyandotSilva@gmail.com>
2019-05-06 20:32:12 -0400

    Add apply example

--- this.js
+++ this.js
@@ -18,4 +18,5 @@ friend.greet(); // this.name = david
 
 const greet = person.greet.bind({ name: 'this is a bound object' });
 person.greet.call({ name: 'this is a bound object' });
+person.greet.apply({ name: 'this is a bound object' });
 greet();
